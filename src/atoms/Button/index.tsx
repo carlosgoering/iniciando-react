97c496ff24e8b5a600d45dc7e3e22b90664114c3
Carlos Augusto Goering <carlosgoeringdev@outlook.com>
2022-06-19 15:11:45 -0300

    primeiro commit

---
+++ src/atoms/Button/index.tsx
@@ -0,0 +1,7 @@
+import * as S from './styles' 
+
+const Button = () => {
+    return <S.Container>Enquary</S.Container>
+}
+
+export default Button;
---
+++ src/molecules/Logo/index.tsx
@@ -0,0 +1,7 @@
+import LogoHero from '../../assets/logo.svg'
+
+const Logo = () => {
+    return <img src={LogoHero} alt="Logo K. com fundo azul e bordas arredondadas." />
+}
+
+export default Logo;
